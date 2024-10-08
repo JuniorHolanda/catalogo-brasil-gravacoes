a52b277b8cf4ea45a1b2ec4a02c36988b1d3d1c3
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-10-08 00:16:55 -0300

    contador de cores e quantidade

--- assets/js/calcOrcamento.js
+++ assets/js/calcOrcamento.js
@@ -1,20 +1,61 @@
 const listArrowsCalc = document.querySelectorAll('.controller__arrows');
+let color = 1
+let amount = 1
+
+const btnAmount = document.querySelector('#btnGravacao');
 const btnCores = document.querySelector('#btnCores');
-const btnGravacao = document.querySelector('#btnGravacao');
+
+
+btnCores.innerHTML = color
+
+function calculateResult(arrow, type) {
+
+
+
+    if (arrow === "increment" && type === "color") {
+        if (color == 5) {
+            color=0
+        }
+        color++
+        btnCores.innerHTML = color
+
+    } else if (arrow === "increment" && type === "amount") {
+        if (amount == 2) {
+            amount=0
+        }
+        amount++
+        btnAmount.innerHTML = amount
+    } else if (arrow === "decrement" && type === "color") {
+        if (color > 1 && color <= 5) {
+            color--
+            btnCores.innerHTML = color
+        } else {
+            color = 5
+            btnCores.innerHTML = color
+        }
+    } else if (arrow === "decrement" && type === "amount") {
+        if (amount > 1) {
+            amount--
+            btnAmount.innerHTML = amount
+        } else if (amount < 2) {
+            amount++
+            btnAmount.innerHTML = amount
+        }
+    }
+}
+
+export function calcOrcamento() {
+    listArrowsCalc.forEach((btnArrow) => {
+        const arrow = btnArrow.dataset.arrow;
+        const type = btnArrow.dataset.type;
+        btnArrow.addEventListener('click' , () => calculateResult(arrow, type))
+    });
+}
+
+
+
 
 
 function pt(p) {
     console.log(p)
-}
-
-console.log(btnGravacao.value)
-
-function calculateResult(index) {
-    let arrow = 
-}
-
-export function calcOrcamento() {
-    listArrowsCalc.forEach((btnArrow, index) => {
-        btnArrow.addEventListener('click' , calcOrcamento())
-    });
 }
