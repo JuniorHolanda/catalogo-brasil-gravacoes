7307aa244c5860e006c7257c7c1b8059acf2a6ca
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-11-20 18:10:19 -0300

    novo estilo

--- assets/js/modules/createBlockHtml.js
+++ assets/js/modules/createBlockHtml.js
@@ -4,14 +4,13 @@ export class CreateBlockHtml {
         this.dinamicId = dinamicId,
         this.title = this.determineTitle(this.category),
         this.subTitle = this.determineSubTitle(this.category),
-        this.description = this.determineDescription(this.category),
         this.measure = this.determineMeasure(this.category)
     }
 
     determineMeasure(category) {
         const descriptionMap = {
             'tampografia': '20x10',
-            'serigrafia': '20x30'
+            'serigrafia': '20x29'
         };
         return descriptionMap[category] || 'Categoria não encontrada';
     }
@@ -32,26 +31,27 @@ export class CreateBlockHtml {
         return descriptionMap[category] || 'Categoria não encontrada';
     }
 
-    determineDescription(category) {
-        const descriptionMap = {
-            'tampografia': 'Personalização com alta qualidade em detalhes e contornos. <br>Para orçamentos em peças grandes, solicite um <a class="description__link" href="https://api.whatsapp.com/send?phone=551132270968">orçamento personalizado</a>',
-            'serigrafia': 'A serigrafia é uma técnica de personalização que traz cores vivas e durabilidade para brindes personalizados. Ideal para destacar a marca em diversos tipos de materiais com alta qualidade de impressão'
-        };
-        return descriptionMap[category] || 'Categoria não encontrada';
-    }
-
     renderBlockHtml() {
         return `
         <section class="blockHtml">
-            <div class="description">
-                <h2 class="description__title">${this.title}</h2>
-                <h3 class="description__sub-title">${this.subTitle}</h3>
-                <p class="description__text">${this.description}</p>
-            </div>
-            
-            <div class="carousel">
-                <div class="carousel__content" id="${this.dinamicId}">
-                <!-- inserção dos cards via carousel.js -->
+            <div class="container-main">
+                <div class="description">
+                    <div class="description-header">
+                        <h2 class="description__title">${this.title}</h2>
+                        <h3 class="description__sub-title">${this.subTitle}</h3>
+                    </div>
+                    <div class="contoller__info">
+                        <p class="controller__text">
+                            Tamanho máximo do logo <br><strong class="controller-text-strong">${this.measure}cm</strong>
+                        </p>
+                        <ul class="controller__container-reference" id="containerReference${this.title}"></ul>
+                    </div>
+                </div>
+                
+                <div class="carousel">
+                    <div class="carousel__content" id="${this.dinamicId}">
+                    <!-- inserção dos cards via carousel.js -->
+                    </div>
                 </div>
             </div>
             
@@ -71,11 +71,6 @@ export class CreateBlockHtml {
                         <i class="controller__arrows controller__arow-down fa-solid fa-angle-down" data-arrow="decrement" data-type="Amount" data-component="${this.title}"></i>
                     </div>
                     
-                </div>
-                <div class="contoller__info">
-                    <h2 class="controller__title">Referência</h2>
-                    <p class="controller__text">Tamanho máximo do logo ${this.measure}cm</p>
-                    <ul class="controller__container-reference" id="containerReference${this.title}"></ul>
                 </div>
 
                 <ul class="controller__container-price" id="containerListPrice${this.title}"></ul>
